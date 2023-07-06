9d6c88b43d8018a8c6d194d887ee73ffb306b440
Steve Adrien <montheadrien@gmail.com>
2023-07-06 13:13:36 +0100

    Voge Update

--- app/Models/Bcategory.php
+++ app/Models/Bcategory.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Translatable\HasTranslations;
 
 class Bcategory extends Model
 {
     use HasFactory;
+    use HasTranslations;
+    use SoftDeletes;
+
+    public $translatable = ['name', 'description'];
+    protected $fillable = ['codename', 'name', 'description', 'short_description', 'deleted_at', 'meta_description', 'meta_keywords', 'meta_image_path', 'image_path'];
+
+    public function blogs()
+    {
+        return $this->hasMany(Blog::class);
+    }
 }
