e5ca52eaba44ccd2724d1082bfde9ccde7d25602
Steve Adrien <montheadrien@gmail.com>
2023-07-07 11:08:53 +0100

    Voge Update

--- resources/views/frontend/pages/about.blade.php
+++ resources/views/frontend/pages/about.blade.php
@@ -140,30 +140,21 @@
             <div class="col-md-6 order-2 order-md-1 text-center text-md-start">
                 <div class="owl-carousel owl-theme nav-style-1 nav-center-images-only stage-margin mb-0" data-plugin-options="{'responsive': {'576': {'items': 1}, '768': {'items': 1}, '992': {'items': 2}, '1200': {'items': 2}}, 'margin': 25, 'loop': false, 'nav': true, 'dots': false, 'stagePadding': 40}">
                     <div>
-                        <img class="img-fluid rounded-0 mb-4" src="/frontend/img/team/team-1.jpg" alt="" />
-                        <h3 class="font-weight-bold text-color-dark text-4 mb-0">John Doe</h3>
-                        <p class="text-2 mb-0">CEO</p>
+                        <img class="img-fluid rounded-0 mb-4" src="/frontend/img/team/team-1.jpeg" alt="" />
+                        <h3 class="font-weight-bold text-color-dark text-4 mb-0">Pates Linguines</h3>
+                        <p class="text-2 mb-0">Responsable Juridique</p>
                     </div>
                     <div>
                         <img class="img-fluid rounded-0 mb-4" src="/frontend/img/team/team-2.jpg" alt="" />
-                        <h3 class="font-weight-bold text-color-dark text-4 mb-0">Jessica Doe</h3>
-                        <p class="text-2 mb-0">CEO</p>
+                        <h3 class="font-weight-bold text-color-dark text-4 mb-0">Jules Berlin NDJEUGA</h3>
+                        <p class="text-2 mb-0">Promoteur et Président</p>
                     </div>
                     <div>
                         <img class="img-fluid rounded-0 mb-4" src="/frontend/img/team/team-3.jpg" alt="" />
-                        <h3 class="font-weight-bold text-color-dark text-4 mb-0">Chris Doe</h3>
-                        <p class="text-2 mb-0">DEVELOPER</p>
+                        <h3 class="font-weight-bold text-color-dark text-4 mb-0">BIADOU Darling épouse MELINGUI</h3>
+                        <p class="text-2 mb-0">Chargé de communication</p>
                     </div>
-                    <div>
-                        <img class="img-fluid rounded-0 mb-4" src="/frontend/img/team/team-4.jpg" alt="" />
-                        <h3 class="font-weight-bold text-color-dark text-4 mb-0">Julie Doe</h3>
-                        <p class="text-2 mb-0">SEO ANALYST</p>
-                    </div>
-                    <div>
-                        <img class="img-fluid rounded-0 mb-4" src="/frontend/img/team/team-5.jpg" alt="" />
-                        <h3 class="font-weight-bold text-color-dark text-4 mb-0">Robert Doe</h3>
-                        <p class="text-2 mb-0">DESIGNER</p>
-                    </div>
+
                 </div>
             </div>
             <div class="col-md-6 order-1 order-md-2 text-center text-md-start mb-5 mb-md-0">
