2dc91cb0bb38f2c1a9f6f3044ad6f7c6ca9d32e7
Steve Adrien <montheadrien@gmail.com>
2023-07-07 11:09:53 +0100

    Voge Update

--- resources/views/frontend/pages/about.blade.php
+++ resources/views/frontend/pages/about.blade.php
@@ -165,58 +165,6 @@
         </div>
     </div>
 
-    <section class="section bg-color-grey-scale-1 section-height-3 border-0 m-0">
-        <div class="container pb-2">
-            <div class="row">
-                <div class="col-lg-6 text-center text-md-start mb-5 mb-lg-0">
-                    <h2 class="text-color-dark font-weight-normal text-6 mb-2">About <strong class="font-weight-extra-bold">Our Clients</strong></h2>
-                    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit massa enim. Nullam id varius nunc.</p>
-                    <div class="row justify-content-center my-5">
-                        <div class="col-8 text-center col-md-4">
-                            <img src="/frontend/img/logos/logo-1.png" class="img-fluid hover-effect-3" alt="" />
-                        </div>
-                        <div class="col-8 text-center col-md-4 my-3 my-md-0">
-                            <img src="/frontend/img/logos/logo-2.png" class="img-fluid hover-effect-3" alt="" />
-                        </div>
-                        <div class="col-8 text-center col-md-4">
-                            <img src="/frontend/img/logos/logo-3.png" class="img-fluid hover-effect-3" alt="" />
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="owl-carousel owl-theme nav-style-1 stage-margin" data-plugin-options="{'responsive': {'576': {'items': 1}, '768': {'items': 1}, '992': {'items': 1}, '1200': {'items': 1}}, 'loop': true, 'nav': false, 'dots': false, 'stagePadding': 40, 'autoplay': true, 'autoplayTimeout': 6000, 'loop': true}">
-                        <div>
-                            <div class="testimonial testimonial-style-2 testimonial-with-quotes testimonial-quotes-dark testimonial-remove-right-quote ps-md-4 mb-0">
-                                <div class="testimonial-author">
-                                    <img src="/frontend/img/clients/client-1.jpg" class="img-fluid rounded-circle mb-0" alt="">
-                                </div>
-                                <blockquote>
-                                    <p class="text-color-dark text-4 line-height-5 mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae metus tellus. Curabitur non lorem at odio tempus consectetur vel eu lacus. Morbi.</p>
-                                </blockquote>
-                                <div class="testimonial-author">
-                                    <p><strong class="font-weight-extra-bold text-2">John Smith</strong><span>Okler</span></p>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div class="testimonial testimonial-style-2 testimonial-with-quotes testimonial-quotes-dark testimonial-remove-right-quote ps-md-4 mb-0">
-                                <div class="testimonial-author">
-                                    <img src="/frontend/img/clients/client-1.jpg" class="img-fluid rounded-circle mb-0" alt="">
-                                </div>
-                                <blockquote>
-                                    <p class="text-color-dark text-4 line-height-5 mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae metus tellus. Curabitur non lorem at odio tempus consectetur vel eu lacus. Morbi.</p>
-                                </blockquote>
-                                <div class="testimonial-author">
-                                    <p><strong class="font-weight-extra-bold text-2">John Smith</strong><span>Okler</span></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
     @include('frontend.layouts.recent-blogs-section')
 
 @endsection
