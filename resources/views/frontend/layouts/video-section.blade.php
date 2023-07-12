9814b79a4b0dbc75367ef081c24bdcdf37b65d3e
Steve Adrien <montheadrien@gmail.com>
2023-07-12 18:44:47 +0100

    Voge Update

---
+++ resources/views/frontend/layouts/video-section.blade.php
@@ -0,0 +1,30 @@
+<div style="background-color: black; color: white">
+    <div class="container my-5" >
+        <div class="row py-5">
+            <div class="col">
+                <div class="row align-items-center">
+                    <div class="col-lg-6">
+                        <div class="featured-boxes featured-boxes-modern-style-1">
+                            <div class="featured-box overlay overlay-show overlay-op-9 border-radius border-0">
+                                <div class="featured-box-background" style="background-image: url(/frontend/img/gallery/gallery-2.jpg); background-size: cover; background-position: center;"></div>
+                                <div class="box-content px-lg-4 px-xl-5 py-lg-5">
+                                    <div class="py-5 my-4">
+                                        <a class="text-decoration-none lightbox" href="https://www.youtube.com/watch?v=Fib_174bZSM&t" data-plugin-options="{'type':'iframe'}">
+                                            <img class="icon-animated" width="60" src="/frontend/vendor/linea-icons/linea-music/icons/play-button.svg" alt="" data-icon data-plugin-options="{'color': '#FFF', 'animated': true, 'delay': 600, 'strokeBased': true}" />
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-5 text-center text-lg-start">
+                        <h4 class="text-6 font-weight-bold line-height-5 mt-4 mt-lg-0" style="color: white">Revivez nos moments forts sur notre chaine youtube</h4>
+                        <p style="color: white">Nous vous encourageons également à soutenir notre travail en appréciant les vidéos, en les partageant avec vos amis et en vous abonnant à notre chaîne. En vous abonnant, vous ne manquerez aucune nouvelle vidéo et vous nous aiderez à continuer à grandir et à partager notre passion pour le football avec le monde entier.</p>
+                        <a href="#" class="d-inline-flex align-items-center btn btn-dark text-color-light font-weight-bold px-4 btn-py-2 text-1 rounded">VOGE FA <i class="fa fa-arrow-right ms-2 ps-1 text-3"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
